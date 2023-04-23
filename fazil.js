cbff1079200af4b613ddba7772684599d4bb3c89
Mohammed-Fazil <53987132+Mohammed-Fazil@users.noreply.github.com>
2023-04-23 18:56:13 +0530

    Update fazil.js

--- fazil.js
+++ fazil.js
@@ -1,1 +1,12 @@
-console.log("Hello EveryOne");
+let str="madam";
+
+let rev="";
+
+for(let i=str.length-1;i>=0;i++)
+{
+  rev=rev+str[i];
+}
+if(rev==str)
+  console.log("It is a Palindrome");
+else
+  console.log("It is not a Palindrome);
